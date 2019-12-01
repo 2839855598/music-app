a5aeeab7e83713f49a8162356056f2dff69c8f39
2839855598 <2839855598@qq.com>
2019-12-01 19:13:54 +0800

    fix problem

--- src/api/interfaces.js
+++ src/api/interfaces.js
@@ -1,6 +1,6 @@
 const ports = {
   development: '/',
-  // 服务器有多个项目时候，production用具体的，只有单个项目时候(以/开头) 
+  // 服务器有多个项目时候,production用具体的,只有单个项目时候(以/开头)
   // production 用 /
   production: 'http://47.105.169.196/music/',
 };
