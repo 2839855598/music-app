c80c8fa8b866ca429380a2ae52b436cfc52cfdb0
2839855598 <2839855598@qq.com>
2019-12-02 15:30:50 +0800

    fix baseurl

--- src/envURL/index.js
+++ src/envURL/index.js
@@ -4,6 +4,9 @@ const modeUrlObj = {
     publicPath: '/',
   },
   production: {
+    // hash模式下用 './', history模式下, 如果部署在服务器根目录(/)下,
+    // 用 '/', 如果部署在服务器子目录下(如: /music/)下, 就用
+    // '/music/'
     publicPath: '/music/',
   },
   test: {
