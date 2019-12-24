5cfcd7d5accc746343ebe4f03fbca654975c8744
2839855598 <2839855598@qq.com>
2019-12-24 17:32:41 +0800

    test ip

--- src/api/interfaces.js
+++ src/api/interfaces.js
@@ -2,6 +2,6 @@ const ports = {
   development: '/',
   // 服务器有多个项目时候,production用具体的,只有单个项目时候(以/开头)
   // production 用 /
-  production: 'http://xujiacheng.online/music/',
+  production: 'http://118.190.211.239/music/',
 };
 export default ports[process.env.NODE_ENV];
